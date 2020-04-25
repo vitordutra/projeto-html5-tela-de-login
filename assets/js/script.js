ebbe683062667a89396ac2697ae51462feb3ea51
João Vitor Dutra <jo.vitor.d@gmail.com>
2020-04-25 20:29:30 -0300

    demonstração de event listeners para o formulário

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,3 +12,32 @@ console.log("O seu email é: " + email);
 // Template string: escrever a variável junto com texto sem concatenar.
 // Sintaxe: string entre crases, ${variável}
 console.log(`O seu email é: ${email}`);
+
+// Acabei de inserir id="btn-submit no botão enviar"
+/* Entendendo cada trecho de código:
+    document: documento html
+    getElementById: encontra o elemento com id btn submit e adiciona
+    addEventListener: quero colocar um evento nesse botão
+    argumentos addEventListener ->
+    (nome do evento, função)
+*/
+document.getElementById("btn-submit").addEventListener("click", (e) => {
+  console.log("o botão foi clicado");
+});
+
+// Se o usuário passou o mouse em alguma área
+document.getElementById("form-login").addEventListener("mouseenter", (e) => {
+  console.log("O mouse está sobre o formulário.");
+});
+
+// Usa um padrão parecido com o CSS
+document
+  .querySelector("#form-login")
+  .addEventListener("mouseleave", (e) =>
+    console.log("O documento está fora do formulário.")
+  );
+
+document.querySelector("#form-login").addEventListener("submit", (e) => {
+  e.preventDefault();
+  console.log("Formulário Enviado! Aqui vai o Ajax");
+});
