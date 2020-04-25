96fdb47aab610267185965281368584668f8e6a2
João Vitor Dutra <jo.vitor.d@gmail.com>
2020-04-25 17:17:59 -0300

    adição de javascript. introdução ao js

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+// const: imutável, constante
+// var: variável global
+// let: variável local -> respeita o escopo.
+// usar mais let e const. var é mais antigo.
+
+const email = "joao@hcode.com.br";
+
+console.log(email);
+console.log("O seu email é: " + email);
